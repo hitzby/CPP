cb988ae7b63841cc859686d5fe2836549864c2a0
hitzby <45208586@qq.com>
2026-02-13 20:14:40 +0800

    第一天

---
+++ 第一天/字符串.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    //C语言风格的字符串
+    char str1[]="hello world";
+    cout<<str1<<endl;
+    //C++风格的字符串
+    string str2="hello C++";
+    cout<<str2<<endl;
+    return 0;
+}
