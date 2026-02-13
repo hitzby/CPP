9766adf1fc73e372c4b2d1d762762fff05ec69fb
hitzby <45208586@qq.com>
2026-02-13 19:35:30 +0800

    第一天

---
+++ 第一天/常量.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#define Day 7//宏定义常量
+using namespace std;
+int main(){
+    cout<<"一周有"<<Day<<"天。"<<endl;
+    const int month=12;//const定义常量
+    cout<<"一年有"<<month<<"个月。"<<endl;
+    return 0;
+}
