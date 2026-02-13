6b60d94d483d8e52a5e5c98aaaa620defaf006cf
hitzby <45208586@qq.com>
2026-02-13 20:32:00 +0800

    第一天

---
+++ 第一天/数据的输入.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    cout<<"你输得是："<<a<<endl;
+    double b;
+    cin>>b;
+    cout<<"你输得是："<<b<<endl;
+    string c;
+    cin>>c;
+    cout<<"你输得是："<<c<<endl;
+    bool d;
+    cin>>d;
+    cout<<"你输得是："<<d<<endl;
+    return 0;
+}
