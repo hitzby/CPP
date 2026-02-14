815b3db1a9679fbef1c51ffbdccfadf94c31ee6f
hitzby <45208586@qq.com>
2026-02-14 14:33:42 +0800

    第二天

--- 第一天/字符串.cpp
+++ 第一天/字符串.cpp
@@ -7,6 +7,8 @@ int main(){
     cout<<str1<<endl;
     //C++风格的字符串
     string str2="hello C++";
+    string nameseed="ABCDE";//string 类似于字符数组
+    str2+=nameseed[0];//在这里是直接往后添加字符，算是和C不同的地方
     cout<<str2<<endl;
     return 0;
 }
